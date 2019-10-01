0fbc0013b7b58f2df70e36d296c1e20b5fec3175
tao <hellotaotao@gmail.com>
2019-10-02 01:09:33 +0930

    error fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name):base(name)
         {
-            Type = GradeBookType.Standard;
+            Type = GradeBookType.Ranked;
         }
     }
 }
