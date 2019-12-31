b6e2ee27ca6440429c99aa9629215b43e69237f8
Tao Wang <tao.wang@certegy.com.au>
2019-12-31 14:55:02 +1030

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -25,5 +25,29 @@ namespace GradeBook.GradeBooks
             if (grades[threshold * 3 - 1] <= averageGrade) return 'C';
             return grades[threshold * 4 - 1] <= averageGrade ? 'D' : 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+            {
+                Console.Write(
+                    "Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+            {
+                Console.Write(
+                    "Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
